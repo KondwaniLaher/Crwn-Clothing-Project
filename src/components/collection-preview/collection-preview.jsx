338df921b7d9881aa51376eda3bf3c7ee95a0bcf
KondwaniLaher <kondwanilaher@yahoo.com>
2021-03-24 20:20:54 +0000

    shop page added, collection preview component and collection item compinent added. styling bug fixed

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -7,7 +7,7 @@ import "./collection-preview.styles.scss";
 const CollectionPreview = ({ title, items }) => (
   <div>
     <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview">
+    <div className="collection-preview">
       {items
         .filter((item, idx) => idx < 4)
         .map(({ id, ...otherItemProps }) => (
